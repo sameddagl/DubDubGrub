1e1ea05ee0981010627b5c4badaa45b59aa2b13b
Samed Dağlı <samedddagl@gmail.com>
2023-05-15 16:38:34 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Samed Dağlı on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
